6ccc5df5100094c4bb217cf02e6ff3c1509f79f9
Marco Burggraf <marco.burggraf@haufe-lexware.net>
2017-09-05 14:52:51 +0200

    introduce pattern state

--- playground/webApp/app/gofPlayground/stateDefinition.js
+++ playground/webApp/app/gofPlayground/stateDefinition.js
@@ -8,22 +8,37 @@ angular.module('myApp.gofPlayground', [
 config(['$locationProvider', '$routeProvider', '$stateProvider',
     function($locationProvider, $routeProvider, $stateProvider) {
         $stateProvider
+
             .state('gofPlayground', {
               abstract: true,
               url: "/playground/gof",
-              controller: "consoleCtrl as console",
               templateUrl: "gofPlayground/gofPlayground.html"
             })
 
             .state('gofPlayground.patterns', {
               url: "/patterns",
               views: {
-                  // 'console@gofPlayground': {
-                  //       templateUrl: "gofPlayground/views/console/console.html"},
                   'headline@gofPlayground': {
                         template: "<h1>Pattern overview</h1><br>" },
                   'content@gofPlayground': {
                         templateUrl: "gofPlayground/views/patterns/patternOverview.html" }
-              }
-        });
+                }
+              })
+
+            .state('pattern', {
+              abstract: true,
+              url: "/playground/gof/pattern",
+              controller: "consoleCtrl as console",
+              templateUrl: "gofPlayground/views/patterns/pattern.html"
+            })
+
+              .state('pattern.example', {
+                url: "/example",
+                views: {
+                   'console@pattern': {
+                         templateUrl: "gofPlayground/views/console/console.html"},
+                   'content@pattern': {
+                        templateUrl: "gofPlayground/views/patterns/patternOverview.html" }
+                }
+              });
 }]);
