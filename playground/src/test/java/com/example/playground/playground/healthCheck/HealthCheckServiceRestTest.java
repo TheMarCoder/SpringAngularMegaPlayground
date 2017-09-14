0ed8f31c7904db9399d8d41db2d58b02f66acebf
Marco Burggraf <marco.burggraf@haufe-lexware.net>
2017-09-14 09:16:17 +0200

    Implrove HealthCheckServiceRestTest

--- playground/src/test/java/com/example/playground/playground/healthCheck/HealthCheckServiceRestTest.java
+++ playground/src/test/java/com/example/playground/playground/healthCheck/HealthCheckServiceRestTest.java
@@ -18,6 +18,8 @@ public class HealthCheckServiceRestTest extends BaseTest {
   public void testHealthCheck() {
     LoggableResponse response = healthCheckServiceRest.healthCheck();
 
+    Assert.assertEquals("Expected 1 log entry while execution of '@GetMapping(value = '/healthCheck'",
+      1, response.getLogEntries().size());
     Assert.assertEquals("Expected that log entry is '@GetMapping(value = '/healthCheck') called'",
       "@GetMapping(value = '/healthCheck') called", response.getLogEntries().iterator().next());
     Assert.assertEquals("Expected that healtheck result content is 'backend healthCheck ... OK'",
